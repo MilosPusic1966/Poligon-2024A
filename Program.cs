f4d521a59c6fe30ececca80d04b75537eef7af25
milos pusic <milos.pusic@prvabeogim.edu.rs>
2024-02-09 20:13:28 +0100

    v0.5

--- Program.cs
+++ Program.cs
@@ -10,9 +10,40 @@ namespace Poligon_2024A
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Unesite temena");
+            /*Console.WriteLine("Unesite temena");
             Console.WriteLine("x0=");
-
+            */
+            Tacka A = new Tacka(3,0);
+            Tacka B = new Tacka(4,-2);
+            Tacka C = new Tacka(5, 0);
+            Tacka D = new Tacka(4, 2);
+            Vektor AB = new Vektor(A, B);
+            Vektor BC = new Vektor(B, C);
+            Vektor CD = new Vektor(C, D);
+            Vektor DA = new Vektor(D, A);
+            Vektor AC = new Vektor(A, C);
+            Vektor BD = new Vektor(B, D);
+            double dAB = AB.duzina();
+            double dBC = BC.duzina();
+            double dCD = CD.duzina();
+            double dDA = DA.duzina();
+            double dBD = BD.duzina();
+            double dAC = AC.duzina();
+            if ((dAB == dBC) && (dBC == dCD) && (dCD == dDA))
+            {
+                if (dBD == dAC)
+                {
+                    Console.WriteLine("Kvadrat");
+                }
+                else
+                {
+                    Console.WriteLine("Romb");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nije pravilan");
+            }
         }
     }
 }
--- Vektor.cs
+++ Vektor.cs
@@ -9,8 +9,8 @@ namespace Poligon_2024A
     internal class Vektor
     {
         Tacka pocetak, kraj;
-        Vektor() { }
-        Vektor(Tacka pocetak, Tacka kraj)
+        public Vektor() { }
+        public Vektor(Tacka pocetak, Tacka kraj)
         {
             this.pocetak = pocetak;
             this.kraj = kraj;
@@ -18,6 +18,17 @@ namespace Poligon_2024A
         public Tacka centriraj()
         {
             return new Tacka(kraj.x - pocetak.x, kraj.y - pocetak.y);
+        }
+        public double duzina()
+        {
+            return centriraj().R();
+        }
+        public static double skalarni(Vektor a, Vektor b)
+        {
+            Tacka prva = a.centriraj();
+            Tacka druga = b.centriraj();
+            double SP = prva.x * druga.x + prva.y * druga.y;
+            return SP;
         }
     }
 }
