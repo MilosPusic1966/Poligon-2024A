217b805b2607970084fe9a08c10b39e86506e506
milos pusic <milos.pusic@prvabeogim.edu.rs>
2024-02-09 19:22:28 +0100

    v.02

--- Tacka.cs
+++ Tacka.cs
@@ -8,5 +8,16 @@ namespace Poligon_2024A
 {
     internal class Tacka
     {
+        public double x, y;
+        public Tacka() { }
+        public Tacka(double _x, double _y) 
+        {
+            x = _x;
+            y = _y;
+        }
+        public double R()
+        {
+            return Math.Sqrt(x*x + y*y);
+        }
     }
 }
--- Vektor.cs
+++ Vektor.cs
@@ -8,5 +8,16 @@ namespace Poligon_2024A
 {
     internal class Vektor
     {
+        Tacka pocetak, kraj;
+        Vektor() { }
+        Vektor(Tacka pocetak, Tacka kraj)
+        {
+            this.pocetak = pocetak;
+            this.kraj = kraj;
+        }
+        public Tacka centriraj()
+        {
+            return new Tacka(kraj.x - pocetak.x, kraj.y - pocetak.y);
+        }
     }
 }
